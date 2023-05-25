06c8ca09aedd340ca01f06a5fcaaaa0cee5166d2
horozal <36187605+horozal@users.noreply.github.com>
2023-05-25 23:30:27 +0200

    Update entity_controller.py

--- app/controllers/entity_controller.py
+++ app/controllers/entity_controller.py
@@ -1,6 +1,5 @@
 from app.models.entity import Entity
 
 class EntityController(Entity):
-  
- def request(self) -> str:
-  return f"Entity controller"
+    def request(self) -> str:
+        return f"Entity controller"
